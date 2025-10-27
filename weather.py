ac69ee7989b3071df2f1ed790274f9bb41771fff
Paul Smith <paulsmith1044@gmail.com>
2025-10-27 19:09:20 -0400

    Rename main.py -> weather.py, implement CLI arguments

--- weather.py
+++ weather.py
@@ -1,24 +1,17 @@
 import requests
+import sys
 
-input_city = "Kelowna, BC"
+input_city = sys.argv[1]
 geocoding_url = "https://nominatim.openstreetmap.org/search"
 geocoding_params = {"q": input_city, "format": "json", "addressdetails": "1"}
-
-city_char_list = []
-
-for i in input_city:
-    if i == " ":
-        city_char_list.append("+")
-    else:
-        city_char_list.append(i)
-
-city_parsed = "".join(city_char_list)
+geocoding_headers = {"User-Agent": "OpenStreetMeteo/1.0"}
 
 #lat = None
 #long = None
 #weather_url = "https://api.open-meteo.com/v1/forecast?"
 #weather_params = {"latitude": lat, "longitude": long, "current": "temperature_2m&wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"}
 
-geocoding_json = requests.get(url=geocoding_url, params=geocoding_params)
+geocoding_request = requests.get(url=geocoding_url, params=geocoding_params, headers=geocoding_headers)
+geocoding_json = geocoding_request.json()
 
 print(geocoding_json)
