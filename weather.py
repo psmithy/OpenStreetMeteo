114098199ab53bd7a1b409bdc52f8ba76009819d
Paul Smith <paulsmith1044@gmail.com>
2025-10-27 20:57:43 -0400

    Complete API requests

--- weather.py
+++ weather.py
@@ -1,17 +1,31 @@
-import requests
 import sys
+import requests
 
 input_city = sys.argv[1]
 geocoding_url = "https://nominatim.openstreetmap.org/search"
-geocoding_params = {"q": input_city, "format": "json", "addressdetails": "1"}
+geocoding_params = {
+    "q": input_city,
+    "format": "json",
+    "addressdetails": "1"}
 geocoding_headers = {"User-Agent": "OpenStreetMeteo/1.0"}
-
-#lat = None
-#long = None
-#weather_url = "https://api.open-meteo.com/v1/forecast?"
-#weather_params = {"latitude": lat, "longitude": long, "current": "temperature_2m&wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"}
 
 geocoding_request = requests.get(url=geocoding_url, params=geocoding_params, headers=geocoding_headers)
 geocoding_json = geocoding_request.json()
+geocoding_dict = geocoding_json[0]
+lat = geocoding_dict["lat"]
+lon = geocoding_dict["lon"]
 
-print(geocoding_json)
+weather_url = "https://api.open-meteo.com/v1/forecast"
+weather_params = {
+    "latitude": lat,
+	"longitude": lon,
+	"hourly": "temperature_2m",
+    "timezone": "auto",
+    "forecast_days": 1
+}
+
+weather_request = requests.get(url=weather_url, params=weather_params)
+weather_json = weather_request.json()
+weather_dict = weather_json[0]
+
+print(weather_dict)
